f45de17adf699c5f9186fa787e74908aa6499ba7
Benson Mburu <nguruben@gmail.com>
2015-10-13 17:33:21 +0300

    register page

---
+++ app/src/main/java/ml/dev2dev/myapp/Register.java
@@ -0,0 +1,19 @@
+package ml.dev2dev.myapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by benson on 10/13/15.
+ */
+public class Register extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reigister);
+        
+    }
+}
