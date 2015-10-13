b28dcece00f532b7d33092ff8b1f578c3859c954
Benson Mburu <nguruben@gmail.com>
2015-10-13 18:48:26 +0300

    register Event

--- app/src/main/java/ml/dev2dev/myapp/Login.java
+++ app/src/main/java/ml/dev2dev/myapp/Login.java
@@ -1,7 +1,9 @@
 package ml.dev2dev.myapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,7 +13,7 @@ import android.widget.TextView;
  */
 public class Login extends Activity {
 
-    //Define the views
+    //Declare the views login page
 
     EditText username,password;
     Button Login,Register;
@@ -23,13 +25,20 @@ public class Login extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+//assign variables
         username=(EditText)findViewById(R.id.username);
         password=(EditText)findViewById(R.id.password);
         Login=(Button)findViewById(R.id.login);
         Register=(Button)findViewById( R.id.register);
         forgotpassword=(TextView)findViewById(R.id.forg);
-
+     Register.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             Intent R=new Intent(Login.this,Register.class);
+             startActivity(R);
+             finish();
+         }
+     });
     }
 
 
